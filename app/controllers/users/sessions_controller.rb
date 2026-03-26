b769bf9d85ecf500a9a1d4f175f1ed33416206b3
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-03-26 19:27:23 +0900

    fix: resolve Zeitwerk autoload path for Users::SessionsController

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,4 @@
-# app/controllers/users/sessions_controller.rb
-class Users::SessionsController < Devise::SessionsController
-    def guest_sign_in
-        user = User.find_or_create_by!(email: "guest@example.com") do |user|
-            user.password = SecureRandom.urlsafe_base64
-        end
-        sign_in user
-        redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
-    end
+module Users
+  class SessionsController < Devise::SessionsController
+  end
 end
