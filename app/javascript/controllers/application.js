1a57946c2d32b3d5792bd8b2d97bc9b505c62bdb
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-04-16 02:00:23 +0900

    fix stimulus controllers setup

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 // app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
 
 application.debug = false
 window.Stimulus = application
-
-eagerLoadControllersFrom("controllers", application)
 
 export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,4 @@
+// app/javascript/controllers/index.js
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
