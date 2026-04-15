cb29dfb8a2844afce488189cde4469078d6d04fe
mirai <249426312+miraisato-dev@users.noreply.github.com>
2026-04-16 01:36:26 +0900

    Update application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 // app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
 
 application.debug = false
 window.Stimulus = application
+
+eagerLoadControllersFrom("controllers", application)
 
 export { application }
