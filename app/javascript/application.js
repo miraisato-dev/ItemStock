9449ba7ce096514303b0ee6ac4cf2b80aab954d1
mirai <249426312+miraisato-dev@users.noreply.github.com>
2026-04-16 00:41:30 +0900

    Refactor imports in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
-// app/javascript/application.js
 import "@hotwired/turbo-rails"
 import "controllers"
+
 import "chartkick"
 import "chart.js"
-import "./item"
+
+import "item"
+import "image_upload"
 
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
-
-import "./image_upload"
