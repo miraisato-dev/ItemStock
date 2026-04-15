da6875d15377edd8283647f6436debff0157a23a
mirai <249426312+miraisato-dev@users.noreply.github.com>
2026-04-16 01:28:52 +0900

    Remove unused import in application.js
    Removed unused import statement for 'controllers'.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,8 @@
 // app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
-import "controllers"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
 application.debug = false
 window.Stimulus = application
 
