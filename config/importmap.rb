2d5e04632e878af424c362213b7a2e9a7e86a841
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-04-17 22:22:13 +0900

    fix: importmap resolution

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # config/importmap.rb
 
-pin "application", preload: true
+pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
