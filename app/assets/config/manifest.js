89254792b77bcea669976518ef41d14c90413321
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-04-17 23:51:51 +0900

    fix importmap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,4 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link application.js
-//= link item.js
-//= link image_upload.js
+//= link_tree ../../javascript .js
