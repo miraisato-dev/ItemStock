841421c1eb08d78b819166c8b1b692248fccbf12
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-04-18 00:39:22 +0900

    Fix manifest to include full javascript tree

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link application.js
-//= link_tree ../../javascript/controllers .js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
