c91e5f52e1fbbde7560b1e65c5622019282586fa
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-04-18 00:03:30 +0900

    Cleanup and prepare for final deploy

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 // app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
+//= link application.js
+//= link_tree ../../javascript/controllers .js
