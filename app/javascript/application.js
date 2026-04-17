bfa91221a2f8e37467681f3338775bf4829c7f7c
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-04-18 00:50:18 +0900

    Final fix for script paths

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,8 +5,8 @@ import "controllers"
 import "chartkick"
 import "chart.js"
 
-import "controllers/item"
-import "controllers/image_upload"
+import "item"
+import "image_upload"
 
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-# これが全ての controllers フォルダ内のファイルを「controllers/名前」で登録します
+# 全ての controllers フォルダ内のファイルを「controllers/名前」で登録
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "@rails/activestorage", to: "activestorage.esm.js"
@@ -12,3 +12,6 @@ pin "chartkick", to: "https://ga.jspm.io/npm:chartkick@5.0.1/dist/chartkick.js"
 pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.0/dist/chart.js"
 pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.2/modular/sortable.esm.js"
+
+pin "item"
+pin "image_upload"
