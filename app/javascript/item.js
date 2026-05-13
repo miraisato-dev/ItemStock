19c84e2547d373b7c9bd79e4d1253f7cbfc26a26
Mirai Sato <249426312+miraisato-dev@users.noreply.github.com>
2026-05-13 21:45:38 +0900

    小さい画像クリック時にサムネイル反映にラグがあることを解決するためにJQueryからJSコードに変更

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,8 +1,19 @@
 // app/javascript/item.js
 // app/views/items/_item.html.erb でサムネイル画像をクリックしたときに、メイン画像を切り替えるための JavaScript コード
-$(function () {
-    $(".thumbnail").on("click", function () {
-        const src = $(this).attr("src");
-        $("#main-image").attr("src", src);
+// $(function () {
+//     $(".thumbnail").on("click", function () {
+//         const src = $(this).attr("src");
+//         $("#main-image").attr("src", src);
+//     });
+// });
+
+document.addEventListener("turbo:load", () => {
+    const thumbnails = document.querySelectorAll(".thumbnail");
+    const mainImage = document.getElementById("main-image");
+
+    thumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener("click", () => {
+            mainImage.src = thumbnail.src;
+        });
     });
 });
